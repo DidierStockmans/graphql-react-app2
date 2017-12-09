2e9101fa6396ec0b3f76acc2e3c1493549edbccc
Didier Stockmans <stockmans.didier@gmail.com>
2017-12-09 16:23:00 +0100

    Added components to show list, create a song.

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://dbuser:dbpass@ds131546.mlab.com:31546/lyricaldb343';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
