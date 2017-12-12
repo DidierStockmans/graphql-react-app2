1a9d6891cdbbf805f19dc99b8572168c74658fb1
Didier Stockmans <stockmans.didier@gmail.com>
2017-12-12 20:55:46 +0100

    Added Song and Lyric components.

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class LyricCreate extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { content: '', songId: this.props.songId };
+    }
+    
+    onSubmit(event) {
+        event.preventDefault();
+
+        const { songId, content } = this.state;
+
+        this.props.mutate({
+            variables: {
+                songId: songId,
+                content: content
+            }
+        }).then(() => this.setState({content: ''}));
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit.bind(this)}>
+                <label>Add a lyric:</label>
+                <input value={this.state.content} onChange={event => this.setState({content: event.target.value})} />
+            </form>
+        );
+    }
+}
+
+const mutation = gql`
+mutation AddLyrics($content: String!, $songId: ID!){
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      lyrics {
+        id
+        content
+      }
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricCreate);
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -8,14 +8,15 @@ import query from './../queries/fetchQuery';
 class SongList extends Component {
 
     onSongDelete(id) {
-        this.props.mutate({variables: { id }});
+        this.props.mutate({variables: { id }})
+            .then(() => this.props.data.refetch());
     }
 
     renderSongs() {
         return this.props.data.songs.map(({id, title}) => {
             return (
                 <li key={id} className="collection-item">
-                    {title}
+                    <Link to={`/songs/${id}`}>{title}</Link>
                     <i className="material-icons" onClick={() => this.onSongDelete(id)}>delete</i>
                 </li>
             );
