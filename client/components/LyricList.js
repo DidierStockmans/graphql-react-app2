c91aa2a510c4dde5bec8763a72a38d78d9db4c32
Didier Stockmans <stockmans.didier@gmail.com>
2017-12-14 20:51:43 +0100

    Added a like button. Added an optimisticResponse.

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,9 +1,34 @@
 import React ,  { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
+    
+    onLike(id, likes) {
+        this.props.mutate({
+            variables: {id},
+            optimisticResponse: {
+                __typeName: 'Mutation',
+                likeLyric: {
+                    __typeName: 'LyricType',
+                    id,
+                    likes: likes + 1
+                }
+            }
+        });
+    }
+
     renderLyrics() {
-        return this.props.lyrics.map(({id, content}) => {
-            return <li key={id} className="collection-item">{content}</li>;
+        return this.props.lyrics.map(({id, content, likes}) => {
+            return (
+                <li key={id} className="collection-item">
+                    {content}
+                    <div className="vote-box">
+                        <i onClick={() => this.onLike(id, likes)} className="material-icons">thumb_up</i>
+                        {likes}
+                    </div>
+                </li>
+            );
         });
     }
 
@@ -16,4 +41,13 @@ class LyricList extends Component {
     }
 }
 
-export default LyricList;
+const mutation = gql`
+    mutation LikeLyric($id: ID!) {
+        likeLyric(id: $id) {
+            id
+            likes
+        }
+    }
+`;
+
+export default graphql(mutation)(LyricList);
